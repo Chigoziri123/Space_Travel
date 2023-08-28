4fe449b458e9d02309fbb78db6f006acdb0e1678
Chigoziri123 <anuna.s@bullseyemanpower.com>
2023-08-28 07:52:39 +0100

    Started work on the navigation bar

--- Space_Travel/space_travel/src/components/Header.js
+++ Space_Travel/space_travel/src/components/Header.js
@@ -7,9 +7,27 @@ export default function Header() {
     <header className="header absolute top-0 left-0">
       <div>
         <Link to="/">
-          <img src={logo} alt="Space Logo" title="Space Tourism Logo" />
+          <img src={logo} alt="Space Logo" 
+          title="Space Tourism Logo" />
         </Link>
       </div>
+{/* Navigation bar inside the header */}
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/"><span>00</span>Home</Link>
+          </li>
+          <li>
+            <Link to="/destination"><span>01</span>Destination</Link>
+          </li>
+          <li>
+            <Link to="/crew"><span>02</span>Crew</Link>
+          </li>
+          <li>
+            <Link to="/technology"><span>03</span>Technology</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   </>
   )
