258e2172f2bef63a943b54ce2377a7ed27c0701c
Samuel~ <chigozirianuna@gmail.com>
2023-08-28 14:14:50 +0100

    Made navigation bar functional

--- Space_Travel/space_travel/src/components/Header.js
+++ Space_Travel/space_travel/src/components/Header.js
@@ -1,33 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link, useLinkClickHandler } from "react-router-dom"
 import logo from "../starter-code/assets/shared/logo.svg"
+import { CgMenuLeft } from "react-icons/cg"
 
 export default function Header() {
+
+  const handleClick =()=> {
+    const navbar = document.querySelector(".navbar")
+    const listItems = document.querySelectorAll(".list-item")
+
+    navbar.classList.toggle("open")
+    // If the toggle is set to the class of "open" in the nav-bar, when each 
+    // of the list items is clicked,  the classList disappears.
+
+    listItems.forEach((item) => 
+    item.addEventListener("click", () => navbar.classList.remove("open"))
+    )
+  }
   return (
   <>
-    <header className="header absolute top-0 left-0">
+    <header className="header flex items-center justify-between w-full lg: flex-row">
       <div>
         <Link to="/">
           <img src={logo} alt="Space Logo" 
           title="Space Tourism Logo" />
         </Link>
       </div>
-{/* Navigation bar inside the header */}
+      {/* Navigation bar inside the header */}
       <nav className="navbar">
         <ul>
-          <li>
-            <Link to="/"><span>00</span>Home</Link>
+          <li className="list-item">
+            <Link to="/" className="lg:text-white"><span className="lg:text-4xl lg:font-bold mr-2">00</span>Home</Link>
           </li>
-          <li>
-            <Link to="/destination"><span>01</span>Destination</Link>
+          <li className="list-item">
+            <Link to="/destination" className="lg:text-white"><span className="lg:text-4xl lg:font-bold mr-2">01</span>Destination</Link>
           </li>
-          <li>
-            <Link to="/crew"><span>02</span>Crew</Link>
+          <li className="list-item">
+            <Link to="/crew" className="lg:text-white"><span className="lg:text-4xl lg:font-bold mr-2">02</span>Crew</Link>
           </li>
-          <li>
-            <Link to="/technology"><span>03</span>Technology</Link>
+          <li className="list-item">
+            <Link to="/technology" className="lg:text-white"><span className="lg:text-4xl lg:font-bold mr-2">03</span>Technology</Link>
           </li>
         </ul>
       </nav>
+
+      <div className="lg:hidden">
+        <button onClick={handleClick}>
+          <CgMenuLeft className="text-white text-4xl"/>
+        </button>
+      </div>
     </header>
   </>
   )
