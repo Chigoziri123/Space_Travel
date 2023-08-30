29829f26d3eed0b9982fc87d1a2c6691da7a9c9e
Samuel~ <chigozirianuna@gmail.com>
2023-08-30 18:20:06 +0100

    Worked on the Destination Page and currently working on the Crew Page

--- Space_Travel/space_travel/src/pages/Crew.js
+++ Space_Travel/space_travel/src/pages/Crew.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+import data from "../starter-code/data.json"
+
 export default function Crew() {
+    const [people] = useState(data.crew) 
+    const [value, setValue] = useState(0)
+  
+    if (value < 0 || value >= people.length) {
+      setValue(0)
+    }
+  
+    const {name, images, role, bio} = people[value]
     return (
-      <div>
-          <h1>Crew Page</h1>
-      </div>
+      <>
+        <section className="home destination px-5">
+          <h1>02 Meet your Crew</h1>
+  
+          <div className="md:grid grid-cols-2 md:items-center
+          lg:max-w-7xl lg:mx-auto lg:pt-40">
+            <article>
+              <img src={images.png} alt={name} title={name} 
+              className="block mx-auto w-3/4 mb-10"/>
+            </article>
+  
+            <article className="text-center lg:text-left">
+              {people.map((item, index) => (
+                <button key={index} onClick={() => setValue(index)}
+                className={`uppercase text-white text-2xl pb-2 mx-2 ${index === value && "border-b border-white"}`}>
+                  {item.name}</button>
+              ))}
+              <h2 className="text-5xl font-bold text-white mt-10 uppercase tracking-widest ">{name}</h2>
+              <p className="text-gray-400">{bio}</p>
+            </article>
+          </div>
+        </section> 
+  
+      </>
     )
   }
--- Space_Travel/space_travel/src/pages/Destination.js
+++ Space_Travel/space_travel/src/pages/Destination.js
@@ -1,29 +1,39 @@
 import { useState } from "react"
 import data from "../starter-code/data.json"
 
+
 export default function Destination() {
   const [planets] = useState(data.destinations) 
   const [value, setValue] = useState(0)
 
-  const {name, images, description, distance, travel} = 
-  planets[value]
+  if (value < 0 || value >= planets.length) {
+    setValue(0)
+  }
+
+  const {name, images, description, distance, travel} = planets[value]
   return (
     <>
-      <section>
-        <h1>01 Pick your destination</h1>
+      <section className="home destination px-5">
+        <h1 className="pt-20 lg:pt-40 lg:max-w-7xl lg:mx-auto text-white text-4xl uppercase mb-10">01 Pick your destination</h1>
 
-        <div>
+        <div className="pt-20 md:grid grid-cols-2 md:items-center
+        lg:max-w-7xl lg:mx-auto">
           <article>
-            <img src={`./images/${images.webp}`} alt={name} title={name} />
+            <img src={images.png} alt={name} title={name} 
+            className="block mx-auto w-3/4 mb-10"/>
           </article>
 
-          <article>
-
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <ul>
-              <li>{distance}</li>
-              <li>{travel}</li>
+          <article className="text-center lg:text-left">
+            {planets.map((item, index) => (
+              <button key={index} onClick={() => setValue(index)}
+              className={`uppercase text-white text-2xl pb-2 mx-2 ${index === value && "border-b border-white"}`}>
+                {item.name}</button>
+            ))}
+            <h2 className="text-5xl font-bold text-white mt-10 mb-5 uppercase tracking-widest ">{name}</h2>
+            <p className="text-gray-400 pb-5">{description}</p>
+            <ul className="flex items-center justify-between flex-wrap border-t border-gray-400 pt-10">
+              <li className="text-white text-4xl">Avg. Distance <span className="text-2xl">{distance}</span></li>
+              <li className="text-white text-4xl">Est. Travel Time <span className="text-2xl">{travel}</span></li>
             </ul>
           </article>
         </div>
